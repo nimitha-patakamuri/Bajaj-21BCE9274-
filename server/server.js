96077e47768a249e932d87b692d93e92729e92e4
Nimitha Patakamuri <122997453+nimitha-patakamuri@users.noreply.github.com>
2024-08-25 13:23:31 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
+dotenv.config();
+
+const app = express();
+app.use(cors()); 
+app.use(express.json());
+
+app.use('/bfhl', userRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('MongoDB connection failed:', error.message);
+    });
